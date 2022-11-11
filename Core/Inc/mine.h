ffd10b7f46f1dcae956dd851dc4fecf5c6efa6cf
saiten <saitenntaisei@gmail.com>
2022-11-11 22:19:56 +0900

    Initial Commit

---
+++ Core/Inc/mine.h
@@ -0,0 +1,19 @@
+#ifndef MY_MINE_H
+#define MY_MINE_H
+
+#include "stm32f405xx.h"
+#include "stdio.h"
+#include "stdbool.h"
+#include "stdlib.h"
+#include "stdarg.h"
+#include "math.h"
+#define PI 3.14159
+
+#ifdef __cplusplus
+#include <vector>
+extern "C" {
+#endif /* __cplusplus */
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif
