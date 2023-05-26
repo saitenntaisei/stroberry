dad870600e086d90abb4126d04eb8ea72251c15d
saitenntaisei <saitenntaisei@gmail.com>
2023-05-27 00:48:20 +0900

    fix 1/s

--- Core/Inc/pid.hpp
+++ Core/Inc/pid.hpp
@@ -1,6 +1,7 @@
 #ifndef CORE_INC_PID_HPP_
 #define CORE_INC_PID_HPP_
 #include <parts.hpp>
+#include <queue>
 
 namespace state {
 template <typename T>
@@ -9,6 +10,7 @@ class Pid {
   T Kp, Ki, Kd;
   T error, error_prev, error_sum;
   T output;
+  std::queue<T> que;
 
  public:
   explicit Pid(float Kp, float Ki, float Kd);
@@ -21,12 +23,18 @@ Pid<T>::Pid(float Kp, float Ki, float Kd) : Kp(Kp), Ki(Ki), Kd(Kd) {
   error_prev = 0;
   error_sum = 0;
   output = 0;
+  for (int i = 0; i < 5; i++) {
+    que.push(0);
+  }
 }
 
 template <typename T>
 T Pid<T>::update(T target, T current) {
   error = target - current;
   error_sum += error;
+  error_sum -= que.front();
+  que.pop();
+  que.push(error);
   if (error_sum > 30 * 10000000000) {
     error_sum = 30 * 10000000000;
   } else if (error_sum < -1 * 10000000000) {
