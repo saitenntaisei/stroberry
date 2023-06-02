190169ae6a3c2dc5787a8cad3379b750ff2666e1
saitenntaisei <saitenntaisei@gmail.com>
2023-06-02 23:28:13 +0900

    :recycle: Refactor encoder.hpp

--- Core/Inc/encoder.hpp
+++ Core/Inc/encoder.hpp
@@ -2,6 +2,7 @@
 #define CORE_INC_ENCODER_HPP_
 #include "./mine.hpp"
 namespace pwm {
+constexpr float deg2rad = PI / 180;
 
 template <typename T, typename CNT>
 class Encoder {
@@ -9,18 +10,17 @@ class Encoder {
   const float gear_duty = 10;
   const uint8_t encoder_resolution = 12;
   CNT read_encoder_cnt(void);  // LSB
-  TIM_TypeDef* tim;
-
- public:
+  TIM_TypeDef* tim{};
   T speed_rads;
   T cnt_total;
+
+ public:
   explicit Encoder(TIM_TypeDef* tim);
   T read_encoder_value();  // dps and considered gear duty
+  T get_incremental_degrees();
 };
 template <typename T, typename CNT>
-Encoder<T, CNT>::Encoder(TIM_TypeDef* tim) : tim(tim) {
-  speed_rads = 0;
-  cnt_total = 0;
+Encoder<T, CNT>::Encoder(TIM_TypeDef* tim) : speed_rads(0), cnt_total(0), tim(tim) {
   // HAL_TIM_Encoder_Start(htim, tim_channel);
 }
 
@@ -28,16 +28,21 @@ template <typename T, typename CNT>
 CNT Encoder<T, CNT>::read_encoder_cnt(void) {
   CNT enc_buff = (CNT)tim->CNT;
   tim->CNT = 0;
-  return (CNT)enc_buff;
+  return static_cast<CNT>(enc_buff);
 }
 template <typename T, typename CNT>
-T Encoder<T, CNT>::read_encoder_value() {
-  T encoder_temp = (T)read_encoder_cnt();
+T Encoder<T, CNT>::read_encoder_value() {  // return speed in rads
+  T encoder_temp = static_cast<T>(read_encoder_cnt());
   encoder_temp *= 360;
   encoder_temp /= (gear_duty * encoder_resolution);
   cnt_total += encoder_temp;
-  speed_rads = encoder_temp * 2.0 * PI / 360;  // * control_cycle_Hz;
+  speed_rads = encoder_temp * deg2rad;  // * control_cycle_Hz;
   return speed_rads;
+}
+
+template <typename T, typename CNT>
+T Encoder<T, CNT>::get_incremental_degrees() {
+  return get_incremental_degrees;
 }
 }  // namespace pwm
 #endif  // CORE_INC_ENCODER_HPP_
