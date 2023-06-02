156a7c4500282bf82ae3d62a8ee6e011006ff436
saitenntaisei <saitenntaisei@gmail.com>
2023-06-02 23:45:38 +0900

    :recycle: Refactor pid.hpp

--- Core/Inc/pid.hpp
+++ Core/Inc/pid.hpp
@@ -1,28 +1,26 @@
 #ifndef CORE_INC_PID_HPP_
 #define CORE_INC_PID_HPP_
-#include <parts.hpp>
 #include <queue>
+
+#include "parts.hpp"
 
 namespace state {
 template <typename T>
 class Pid {
  private:
-  T Kp, Ki, Kd;
+  T kp, ki, kd;
   T error, error_prev, error_sum;
   T output;
+  T ts;
   // std::queue<T> que;
 
  public:
-  explicit Pid(float Kp, float Ki, float Kd);
+  explicit Pid(T kp, T ki, T kd, T ts = 0.001);  // NOLINT
   T update(T target, T current);
 };
 
 template <typename T>
-Pid<T>::Pid(float Kp, float Ki, float Kd) : Kp(Kp), Ki(Ki), Kd(Kd) {
-  error = 0;
-  error_prev = 0;
-  error_sum = 0;
-  output = 0;
+Pid<T>::Pid(T kp, T ki, T kd, T ts) : kp(kp), ki(ki), kd(kd), ts(ts), error(0), error_prev(0), error_sum(0), output(0) {
   // for (int i = 0; i < 1000; i++) {
   //   que.push(0);
   // }
@@ -35,7 +33,7 @@ T Pid<T>::update(T target, T current) {
   // error_sum -= que.front();
   // que.pop();
   // que.push(error);
-  output = Kp * error + Ki * error_sum * 0.001f + Kd * (error - error_prev) / 0.001f;
+  output = kp * error + ki * error_sum * ts + kd * (error - error_prev) / ts;
   error_prev = error;
   return output;
 }
