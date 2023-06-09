8b3e68524121bda33dc5427b2d1e3e22bff2096f
saitenntaisei <saitenntaisei@gmail.com>
2023-06-09 14:26:06 +0900

    :sparkles: Buzzer setting

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -343,9 +343,9 @@ void MX_TIM12_Init(void)
 
   /* USER CODE END TIM12_Init 1 */
   htim12.Instance = TIM12;
-  htim12.Init.Prescaler = 0;
+  htim12.Init.Prescaler = 84-1;
   htim12.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim12.Init.Period = 65535;
+  htim12.Init.Period = 10000-1;
   htim12.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim12.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_PWM_Init(&htim12) != HAL_OK)
