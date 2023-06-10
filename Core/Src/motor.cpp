3b92cf62b3341703e9487d0e5c824105d866ffdd
saitenntaisei <saitenntaisei@gmail.com>
2023-06-10 15:25:22 +0900

    :bug: Set limiter to motor driver

--- Core/Src/motor.cpp
+++ Core/Src/motor.cpp
@@ -3,6 +3,11 @@ namespace pwm {
 Motor::Motor(TIM_HandleTypeDef* tim_1, TIM_HandleTypeDef* tim_2, unsigned int channel_1, unsigned int channel_2) : out_1(tim_1, channel_1), out_2(tim_2, channel_2) {}
 void Motor::drive(int16_t duty) {
   // printf("duty: %d\r\n", duty);
+  if (duty >= 1000) {
+    duty = 999;
+  } else if (duty <= -1000) {
+    duty = -999;
+  }
   if (duty >= 0) {
     __HAL_TIM_SET_COMPARE(out_1.tim, out_1.channel, abs(duty));
   } else if (duty < 0) {
